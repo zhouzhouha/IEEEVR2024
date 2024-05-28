3a1670c42ddf07197bee662fd0b249789b40f44d
xuemei <may883520@163.com>
2024-05-28 16:03:12 +0200

    finish the pilot study

---
+++ Assets/Scripts/RatingController.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+using UnityEngine.XR.Interaction.Toolkit;
+using System.IO;
+using System.Text;
+using Unity.VisualScripting;
+
+public class RatingController : MonoBehaviour
+{
+
+    public ActionBasedController rightHandController;
+    private RenderController renderControl;
+    public TextMeshPro textForSliderValue;
+
+    private MainController mainControl;
+    private RenderController renderController;
+    private string dataOutputDir;
+    private string experimentID;
+    private string savePath;
+    private string pc_id;
+    private int pc_index; 
+    public GameObject ButtonBad;
+    //public GameObject ButtonPoor;
+    //public GameObject ButtonFair;
+    //public GameObject ButtonGood;
+    //public GameObject ButtonExcellent;
+
+
+    private bool isFinised = false;
+    public bool FinishedRating
+    {
+        get => isFinised;
+        private set => isFinised = value;
+    }
+
+
+    void Awake()
+    {
+        mainControl = FindObjectOfType<MainController>();
+        if (mainControl == null)
+        {
+            Debug.LogError("Can not get a valid object of MainController!");
+        }
+
+        
+
+        ButtonBad = GameObject.Find("ButtonBad");
+        //ButtonPoor = GameObject.Find("ButtonPoor");
+        //ButtonFair = GameObject.Find("ButtonFair");
+        //ButtonGood = GameObject.Find("ButtonGood");
+        //ButtonExcellent = GameObject.Find("ButtonExcellent");
+
+    }
+
+
+
+    private void OnEnable()
+    {
+        FinishedRating = false;
+        ButtonBad.SetActive(true);
+        //ButtonPoor.SetActive(true);
+        //ButtonFair.SetActive(true);
+        //ButtonGood.SetActive(true);
+        //ButtonExcellent.SetActive(true);
+    }
+
+    //Deactivates the old FillRect and assigns a new one.
+    void Start()
+    {
+        renderControl = FindObjectOfType<RenderController>();
+        if (renderControl == null)
+        {
+            Debug.LogError("Can not get a valid object of RenderController!");
+        }
+
+        dataOutputDir = mainControl.dataSaveDir;
+        experimentID = string.Format("{0}_{1}{2}", mainControl.userid, mainControl.Session, ".txt");
+        savePath = Path.Combine(dataOutputDir, experimentID);
+
+        string pcdpath = renderControl.GetCurrentPcdPath();
+        int pcIndex = renderControl.GetCurrentpcIndex();
+        OnCurrDirPathUpdated(pcdpath, pcIndex);
+        renderControl.OnCurrDirPathUpdated += this.OnCurrDirPathUpdated;
+
+    }
+
+    private void OnCurrDirPathUpdated(string dirpath, int curPCindex)
+    {
+        // get "H5_C1_R5" from "E:\DUMP\H5_C1_R5"
+        string pcdName = Path.GetFileName(dirpath);
+        pc_id = pcdName;
+        pc_index = curPCindex;
+    }
+
+
+    //Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void FinishedRatingFun(int ButtonScore)
+    {
+        Debug.Log("On Click()");
+
+        if (!this.FinishedRating)
+        {
+
+            int rating_score = ButtonScore;
+            string allInfo = "pc_id: " + pc_id + " " + "pc_Index" + pc_index + " " + "MOS: " + rating_score.ToString() + "\n";
+            //SaveRatingScoreButton(allInfo, savePath);
+            FinishedRating = true;
+
+        }
+
+    }
+
+
+
+
+    public void SaveRatingScoreButton(string strs, string path)
+    {
+
+        if (!File.Exists(path))
+        {
+            FileStream fs = File.Create(path);
+            fs.Dispose();
+        }
+
+        using (StreamWriter stream = new StreamWriter(path, true))
+        {
+            stream.WriteLine(strs);
+        }
+
+
+    }
+
+
+
+}
+
+
---
+++ README.md
@@ -0,0 +1,47 @@
+# ISMAR_PointCloud_EyeTracking
+The official repo for QAVA-DPC: Eye-Tracking Based Quality Assessment and Visual Attention Dataset for Dynamic Point Cloud in 6 DoF ISMAR 2023
+# Visual Saliency Map 
+Generated visual saliency map per user can be downloaded via this link: [Visual Saliancy Map](https://zenodo.org/records/10996417)  
+Name Convension of files:
+001_A: uer_session  
+H1_C2_R2_191: stimuli name_codec_distortion level_rotation_degree  
+4246452_rafa_084.txt filename explanation: timestamp_point cloud name_frame_number  
+# Raw Gaze data for 40 users
+In this folder, it includes all the experimental data related to the eye-tracking (in the json file) and the original opinion scores (in two txt files) of each user. It can be downloaded from: [GazeData](https://zenodo.org/records/10996417)  
+user_001 : user_userindex
+001_A.txt: userindex_session.txt  
+20230317-2301_001_A.json:date_userindex_session.json
+
+
+# Visualization:
+This is the video of the H5_C0_R0_BackView  
+
+![Video Visualization](https://github.com/cwi-dis/ISMAR_PointCloud_EyeTracking/blob/main/video/H5_C0_R0_BackView-ezgif.com-crop.gif)   
+
+
+
+and H5_C0_R0_FrontView.  
+
+![Video Visualization](https://github.com/cwi-dis/ISMAR_PointCloud_EyeTracking/blob/main/video/H5_C0_R0_FrontView-ezgif.com-crop.gif)  
+
+### Conditions of use
+
+If you wish to use any of the provided material in your research, we kindly ask you to cite our paper.
+- BibTex
+```
+@INPROCEEDINGS{10316522,
+  author={Zhou, Xuemei and Viola, Irene and Alexiou, Evangelos and Jansen, Jack and Cesar, Pablo},
+  booktitle={2023 IEEE International Symposium on Mixed and Augmented Reality (ISMAR)}, 
+  title={QAVA-DPC: Eye-Tracking Based Quality Assessment and Visual Attention Dataset for Dynamic Point Cloud in 6 DoF}, 
+  year={2023},
+  volume={},
+  number={},
+  pages={69-78},
+  keywords={Point cloud compression;Measurement;Visualization;Solid modeling;Head-mounted displays;Gaze tracking;Inspection;Volumetric video;Dynamic point cloud;Visual saliency;Visual attention;Subjective quality assessment;Objective quality metrics;Eye tracking;6DoF},
+  doi={10.1109/ISMAR59233.2023.00021}}
+```
+## About 
+The QAVA-DPC Dataset is maintained by the Distributed & Interactive Systems (DIS) research group at Centrum Wiskunde & Informatica (CWI).
+
+Contact the authors
+- Xuemei Zhou: xuemei.zhou@cwi.nl
